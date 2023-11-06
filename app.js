034c91d279f0834e7dc2fbba3aee7adbd5fb262e
jihoo529 <u3572038@connect.hku.hk>
2023-11-06 21:39:19 +0800

    login get method

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql2');
+
+var db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'TEST_DB'
+});
+
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+      return;
+    }
+  
+    console.log('Connected to MySQL database');
+  });
+
 
 app.get('/login.html', (req, res) => {
     res.sendFile( __dirname + "/public/" + "login.html" );
@@ -12,12 +30,6 @@ app.get('/process_get', (req, res) => {
     res.send(response);
 })
 
-app.post('/process_post', express.urlencoded({ extended: true }), (req, res) => {
-    var response = "<p>"+req.body.loginName+"</p><p>"+req.body.loginPassword+"</p>";
-    console.log(req.body.loginName)
-    console.log(response);
-    res.send(response);
-})
 
 var server = app.listen(8081, () => {
     var host = server.address().address;
